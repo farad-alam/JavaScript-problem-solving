25e0ea776acb298204d5f89eee599b848baf81a3
Farad Alam <farad.dev@gmail.com>
2025-02-15 11:19:51 +0600

    class inheritance solved

--- 03 class inheritance.js
+++ 03 class inheritance.js
@@ -3,15 +3,29 @@ class Rectangle {
     this.w = w;
     this.h = h;
   }
+
 }
 
 /*
  *  Write code that adds an 'area' method to the Rectangle class' prototype
  */
 
+Rectangle.prototype.area =  function() {
+    let area = this.w * this.h;
+    return area;
+  }
+
 /*
  * Create a Square class that inherits from Rectangle and implement its class constructor
  */
+
+class Square extends Rectangle{
+    constructor(a){
+        super(a, a)
+    }
+}
+
+
 
 if (
   JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) ===
